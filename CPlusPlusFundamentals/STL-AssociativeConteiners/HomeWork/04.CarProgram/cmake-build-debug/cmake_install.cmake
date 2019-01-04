3678aa29f8a3769fb7dd610cd09c73eb7e426f61
Stanislav Bozhikov <bozhikov.stanislav@gmail.com>
2019-01-04 18:18:35 +0200

    New Repo Sot uni CPP

--- CPlusPlusFundamentals/STL-AssociativeConteiners/HomeWork/04.CarProgram/cmake-build-debug/cmake_install.cmake
+++ CPlusPlusFundamentals/STL-AssociativeConteiners/HomeWork/04.CarProgram/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/stanislav/Documents/CLionProjects/CPlusPlusFundamentals/STL-AssociativeConteiners/HomeWork/04.CarProgram
+# Install script for directory: /home/stanislav/GitRepository/Cpp-SoftUni/CPlusPlusFundamentals/STL-AssociativeConteiners/HomeWork/04.CarProgram
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/stanislav/Documents/CLionProjects/CPlusPlusFundamentals/STL-AssociativeConteiners/HomeWork/04.CarProgram/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/stanislav/GitRepository/Cpp-SoftUni/CPlusPlusFundamentals/STL-AssociativeConteiners/HomeWork/04.CarProgram/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
